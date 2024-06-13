97dbd12b2d8c0c599d254409f2c759e1d2b48f5f
Luke Bakken <luke@bakken.io>
2024-06-13 13:47:17 -0700

    still works as expected

--- upgrade.sh
+++ upgrade.sh
@@ -6,6 +6,8 @@ set -o nounset
 curdir="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
 readonly curdir
 
+"$curdir/update_vhost_metadata.sh"
+"$curdir/list_vhost_metadata.sh"
 "$curdir/export_definitions.sh"
 
 echo "[INFO] restarting cluster!"
@@ -15,7 +17,9 @@ for idx in 0 1 2
 do
     svc="rmq$idx"
     echo "[INFO] svc: $svc"
-    docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmq-upgrade drain
+    docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmq-upgrade await_online_quorum_plus_one -t 3600
+    docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmq-upgrade await_online_synchronized_mirror -t 3600
+    docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmqctl stop
     docker compose stop "$svc"
     if (( idx < 2 ))
     then
@@ -23,6 +27,13 @@ do
     fi
     docker compose build --build-arg RABBITMQ_DOCKER_TAG='rabbitmq:3.12-management' "$svc"
     docker compose up --detach --no-deps "$svc"
+    sleep 5
+    docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmqctl await_startup
+    docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmq-diagnostics check_port_connectivity
+    docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmq-diagnostics check_virtual_hosts
 done
 
-"$curdir/import_definitions.sh"
+docker compose exec --no-TTY "$svc" /opt/rabbitmq/sbin/rabbitmqctl enable_feature_flag all
+
+# "$curdir/import_definitions.sh"
+"$curdir/list_vhost_metadata.sh"
